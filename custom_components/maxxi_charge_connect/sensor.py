361cca5236fc3c7b0b3cbe77f5a5cc192725ee67
Frank Klitzing <mephdrac@googlemail.com>
2025-06-04 15:36:27 +0200

    feat: add battery power

--- custom_components/maxxi_charge_connect/sensor.py
+++ custom_components/maxxi_charge_connect/sensor.py
@@ -12,6 +12,8 @@ from .devices.DeviceId import DeviceId
 from .devices.Rssi import Rssi
 from .devices.CcuPower import CcuPower
 from .devices.PvPower import PvPower
+from .devices.BatteryPower import BatteryPower
+
 
 
 _LOGGER = logging.getLogger(__name__)
@@ -24,8 +26,9 @@ async def async_setup_entry(
     rssiSensor = Rssi(entry)
     ccuPowerSensor = CcuPower(entry)
     pvPowerSensor = PvPower(entry)
+    batteryPowerSensor = BatteryPower(entry)
     hello_sensor = HelloWorldSensor(entry)
-    async_add_entities([sensor, rssiSensor, ccuPowerSensor, pvPowerSensor, hello_sensor])
+    async_add_entities([sensor, rssiSensor, ccuPowerSensor, pvPowerSensor, batteryPowerSensor, hello_sensor])
 
 
 class HelloWorldSensor(SensorEntity):

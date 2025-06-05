01bd0afd4e07686fedf00c6bfc7639668a013dc7
Frank Klitzing <mephdrac@googlemail.com>
2025-06-05 12:09:34 +0200

    feat: add energy in kWh for today and total

--- custom_components/maxxi_charge_connect/sensor.py
+++ custom_components/maxxi_charge_connect/sensor.py
@@ -1,25 +1,18 @@
-import logging
-
-from homeassistant.components.sensor import SensorEntity
-from homeassistant.components.text import TextEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import DOMAIN
-from .devices.DeviceId import DeviceId
-from .devices.Rssi import Rssi
-from .devices.CcuPower import CcuPower
-from .devices.PvPower import PvPower
 from .devices.BatteryPower import BatteryPower
 from .devices.BatterySoc import BatterySoc
 from .devices.BatterySoE import BatterySoE
-from .devices.PowerMeter import PowerMeter
+from .devices.CcuPower import CcuPower
+from .devices.DeviceId import DeviceId
 from .devices.FirmwareVersion import FirmwareVersion
-
-
-_LOGGER = logging.getLogger(__name__)
+from .devices.PowerMeter import PowerMeter
+from .devices.PvPower import PvPower
+from .devices.PvTotalEnergy import PvTotalEnergy
+from .devices.Rssi import Rssi
+from .devices.PvTodayEnergy import PvTodayEnergy
 
 
 async def async_setup_entry(
@@ -34,6 +27,8 @@ async def async_setup_entry(
     batterySoE = BatterySoE(entry)
     powerMeter = PowerMeter(entry)
     firmwareVersion = FirmwareVersion(entry)
+    pvTotalEnergy = PvTotalEnergy(entry)
+    pvTodayEnergy = PvTodayEnergy(entry)
 
     async_add_entities(
         [
@@ -46,5 +41,7 @@ async def async_setup_entry(
             batterySoE,
             powerMeter,
             firmwareVersion,
+            pvTotalEnergy,
+            pvTodayEnergy,
         ]
     )
